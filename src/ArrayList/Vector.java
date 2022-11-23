8b5d057367455ac542178308e9424a3332a7500a
Melinatexeira <117683416+Melinatexeira@users.noreply.github.com>
2022-11-23 14:05:30 -0300

    Update Vector.java

--- src/ArrayList/Vector.java
+++ src/ArrayList/Vector.java
@@ -1,2 +1,19 @@
-package ArrayList;public class Vector {
+package ArrayList;
+public class Vector {
+   public static void main(String [] args ) {
+
+        int vector [] = new int[5];
+
+        Scanner entradaEscaner = new Scanner(System.in);
+        for (int i=0; i<vector.length; i++) {
+            System.out.println("Ingrese un número para el índice" + " " + i);
+            vector[i] = entradaEscaner.nextInt();
+        }
+
+
+        for (int i=0; i<vector.length; i++) {
+            System.out.println("[" + i + "]" +  vector[i]);
+
+        }
+    }
 }
